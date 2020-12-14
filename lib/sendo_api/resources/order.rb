1f77be11fc4bbccc0a4f8a70c1d00185e0059992
hewrin <hewrin@hotmail.com>
2020-12-14 17:06:07 +0800

    Fix keyword arguments

--- lib/sendo_api/resources/order.rb
+++ lib/sendo_api/resources/order.rb
@@ -3,11 +3,11 @@ module SendoAPI
     self.element_name = ""
     self.collection_parser = ::SendoAPI::OrderCollection
 
-    def self.find_by_order_number_and_status(order_number: order_number, status: status)
+    def self.find_by_order_number_and_status(order_number:, status:)
       find(:all, from: "/dop/v1/shipment/search/#{order_number}/#{status}")&.first
     end
 
-    def self.update_status(body: body, tracking_number: tracking_number)
+    def self.update_status(body:, tracking_number:)
       connection.post("/dop/v1/shipment/update/#{tracking_number}", JSON.generate(body), headers)
     end
   end
